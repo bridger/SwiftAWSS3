c6dd5ef4ab7dd015d87da38be6ffbe6d49774ef6
Bridger Maxwell <bridger.maxwell@gmail.com>
2017-07-23 16:22:53 -0400

    Point to bridger fork of SwiftAWSSignatureV4

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
 	name: "SwiftAWSS3"
 	,dependencies:[
-	.Package(url:"https://github.com/benspratling4/SwiftAWSSignatureV4.git", majorVersion:0)
+	.Package(url:"https://github.com/bridger/SwiftAWSSignatureV4.git", majorVersion:0)
 	]
 )
